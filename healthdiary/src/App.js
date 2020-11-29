112b4c2d9f812e753d754147947f4ddaf42b50c0
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2020-11-29 16:28:11 +0600

    Started work on Login and Registration page and changed focus to that page

--- healthdiary/src/App.js
+++ healthdiary/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './components/LandingPage/landingPage';
+import { Login } from "./components/Login/index";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Login />
     </>
   );
 }
