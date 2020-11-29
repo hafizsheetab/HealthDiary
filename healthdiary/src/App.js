d8ca5273b4ee72fde96eecfdf9af867721ac3188
JaberHPranto <61055952+JaberHPranto@users.noreply.github.com>
2020-11-29 16:27:33 +0600

    routing added

--- healthdiary/src/App.js
+++ healthdiary/src/App.js
@@ -1,13 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-// import HowItWorks from './components/LandingPage/howItWorks.jsx';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HowItWorks from './components/LandingPage/howItWorks.jsx';
 import LandingPage from './components/LandingPage/landingPage';
+import Login from './components/login';
+import Signup from './components/signup';
 
 function App() {
   return (
     <>
-      <LandingPage />
-      {/* <HowItWorks /> */}
+    <Router>
+      <Switch>
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/howitworks" component={HowItWorks} />
+        <Route exact path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+      
     </>
   );
 }
